6cdb841adc74fe9d306cbc40b5d66be111516fde
Farah Osama <faraho2013@gmail.com>
2024-07-04 11:44:28 -0500

    gpt3 project feature and whatgpt3 files

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
     return (
-        <div>
+        <div className="gpt3__brand section__padding">
             Brand
+            <div>
+                <img src={google} alt="google"></img>
+            </div>
+            <div>
+                <img src={slack} alt="slack"></img>
+            </div>
+            <div>
+                <img src={atlassian} alt="atlassian"></img>
+            </div>
+            <div>
+                <img src={dropbox} alt="dropbox"></img>
+            </div>
+            <div>
+                <img src={shopify} alt="shopify"></img>
+            </div>
         </div>
     )
 }
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import atlassian from '../../assets/atlassian.png'
+import dropbox from '../../assets/dropbox.png'
+import google from '../../assets/google.png'
+import slack from '../../assets/slack.png'
+import shopify from '../../assets/shopify.png'
+
+export {
+    atlassian,
+    dropbox,
+    google,
+    slack,
+    shopify,
+}
